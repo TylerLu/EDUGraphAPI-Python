a13859a0f35fbdb498cf3e35712524ee6cd59bad
TylerLu <admin@tyler.lu>
2017-05-08 15:09:18 +0800

    fix bug 9160: T&S: 'Previous' button doesn't work

--- static/js/users.js
+++ static/js/users.js
@@ -6,7 +6,6 @@ $(document).ready(function () {
     loadImages();
 
     $(".teacher-student .filterlink-container .filterlink").click(function () {
-        search(true);
         var element = $(this);
         element.addClass("selected").siblings("a").removeClass("selected");
         var filterType = element.data("type");
@@ -15,7 +14,6 @@ $(document).ready(function () {
     });
 
     $(".teacher-student .tiles-root-container .pagination .prev, .teacher-student .tiles-root-container .pagination .next").click(function () {
-        search(true);
         var element = $(this);
         if (element.hasClass("current") || element.hasClass("disabled")) {
             return;
@@ -66,11 +64,10 @@ $(document).ready(function () {
                     if (!(value instanceof Array) || value.length == 0) {
                         return;
                     }
-                    content.html("");
                     $.each(value, function (i, user) {
                         var userHtml = '<div class="element ' + (user.is_teacher ? "teacher-bg" : "student-bg") + '">' +
                                            '<div class="userimg">' +
-                                               '<img src="/static/Images/header-default.jpg" realheader="' + '/Photo/UserPhoto/' + user.O365UserId + '" />' +
+                                               '<img src="/static/Images/header-default.jpg" realheader="' + '/Photo/UserPhoto/' + user.object_id + '" />' +
                                            '</div>' +
                                            '<div class="username">' + user.DisplayName + '</div>' +
                                        '</div>';
@@ -100,7 +97,7 @@ $(document).ready(function () {
         var start = (targetPageNum - 1) * 12;
         var end = targetPageNum * 12;
         var elements = content.children();
-        elements.slice(start, end).fadeIn("slow", function () {
+        elements.hide().slice(start, end).fadeIn("slow", function () {
             var img = $(this).find("img[realheader]");
             img.attr("src", img.attr("realheader"));
         });
@@ -109,50 +106,4 @@ $(document).ready(function () {
         prevElement.toggleClass("current", start === 0);
         curPageElement.val(targetPageNum);
     }
-
-    $("#btnsearch").click(function () {
-        search();
-    });
-
-    $('.txtsearch').on('keypress', function (e) {
-        if (e.which === 13) {
-            search();
-        }
-    });
-    function search(isReset) {
-        var queryString;
-        if (isReset) {
-            queryString = "";
-            $(".txtsearch").val("");
-        } else {
-            queryString = $(".txtsearch").val();
-        }
-        var currentFilter = $(".filterlink-container .selected").text().trim().toLocaleLowerCase();
-        var selector = "users";
-        switch (currentFilter)
-        {
-            case "teachers":
-                selector = "teachers";
-                break;
-            case "students":
-                selector = "students";
-                break;
-            default:
-                selector = "users";
-                break;
-        }
-        if (queryString) {
-            $("#"+selector + " .username").each(function () {
-                if ($(this).text().search(new RegExp(queryString, "i")) < 0) {
-                    $(this).closest(".element").hide();
-                } else {
-                    $(this).closest(".element").show();
-                }
-            });
-        }
-        else {
-            $("#"+selector + " .element").show();
-        }
-    }
-
 });
