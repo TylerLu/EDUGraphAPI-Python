b1d357eae7ceb875ea63e4f67ee056c41c519080
TylerLu <admin@tyler.lu>
2017-05-08 14:24:16 +0800

    fix demo helper issues

--- utils/shortcuts.py
+++ utils/shortcuts.py
@@ -3,6 +3,7 @@
  *   * See LICENSE in the project root for license information.  
 '''
 
+import constant
 from django.conf import settings
 from django.shortcuts import render as django_render
 
@@ -14,5 +15,9 @@ def render(request, template_name, context=None, content_type=None, status=None,
         if 'Error' in request.session:
             context['error'] = request.session['Error'].split('\r\n')
             del request.session['Error']      
+        source_code_repository_url = constant.source_code_repository_url
+        if not source_code_repository_url.endswith('/'):
+            source_code_repository_url = source_code_repository_url + '/'
+        context['source_code_repository_url'] = source_code_repository_url        
         context['demo_helper_links'] = settings.DEMO_HELPER.get_links(request.get_full_path())
     return django_render(request, template_name, context, content_type, status, using)
