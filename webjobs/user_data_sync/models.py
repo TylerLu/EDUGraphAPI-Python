13693ec8ad31fbee8062aa895bb47dee572d904b
TylerLu <admin@tyler.lu>
2018-05-04 10:54:40 +0800

    Add code to read data from db

---
+++ webjobs/user_data_sync/models.py
@@ -0,0 +1,49 @@
+import os
+from peewee import MySQLDatabase, Model, BooleanField, CharField, ForeignKeyField
+
+mysql_host = os.environ['MySQLHost']
+mysql_port = int('3306')
+mysql_name = 'edu'
+mysql_user = os.environ['MySQLUser']
+mysql_password = os.environ['MySQLPassword']
+
+db = MySQLDatabase(mysql_name, user=mysql_user, password=mysql_password, host=mysql_host, port=mysql_port)
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+class Organization(BaseModel):
+    # id = AutoField(primary_key=True)
+    name = CharField()
+    tenantId = CharField()
+    isAdminConsented = BooleanField()
+    class Meta:
+        db_table = "organizations"
+
+class Profile(BaseModel):
+    # user = OneToOneField(User, on_delete=None)
+    o365UserId = CharField()
+    o365Email = CharField()
+    jobTitle = CharField()
+    department = CharField()
+    mobilePhone = CharField()
+    organization = ForeignKeyField(Organization, related_name="organizations")
+    class Meta:
+        db_table = "profiles"
+
+db.connect()
+
+organizations = Organization.select() \
+    .where(Organization.isAdminConsented)
+
+for org in organizations:
+    print(org.name)
+
+
+profiles = Profile.select()
+
+for profile in profiles:
+    print(profile.o365Email)
+
+db.close()
