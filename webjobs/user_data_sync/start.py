2d44224c0c76d11516c8c815ee4aecc96a99f8d2
TylerLu <admin@tyler.lu>
2018-05-09 11:15:24 +0800

    Add main method

--- webjobs/user_data_sync/start.py
+++ webjobs/user_data_sync/start.py
@@ -1,11 +1,17 @@
+import sys
 import constants
 from models import database
 from user_data_sync_service import UserDataSyncService
 
+def main():
+    try:
+        database.connect()
+        user_data_sync_service = UserDataSyncService()
+        user_data_sync_service.sync()
+    except:
+        print("Unexpected error: ", sys.exc_info()[0])
+    finally:
+        database.close()
 
-database.connect()
-
-user_data_sync_service = UserDataSyncService()
-user_data_sync_service.sync()
-
-database.close()
+if __name__ == "__main__":
+    main()
